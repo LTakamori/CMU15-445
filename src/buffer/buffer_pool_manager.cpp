94fce836ad5a1b3eb98896d086362b84b8c419df
LTakamori <925951065@qq.com>
2021-02-06 14:38:52 +0800

    finished poj1

--- src/buffer/buffer_pool_manager.cpp
+++ src/buffer/buffer_pool_manager.cpp
@@ -27,7 +27,7 @@ BufferPoolManager::BufferPoolManager(size_t pool_size, DiskManager *disk_manager
   for (size_t i = 0; i < pool_size_; ++i) {
     free_list_.emplace_back(static_cast<int>(i));
   }
-  // todo: free list的内容已经写死为o-i，作为map中的frame_id, 而page_id则可以任意（参考cache）
+  // free list的内容已经写死为o-i，作为map中的frame_id, 而page_id则可以任意（参考cache）
   // free list中记录的是没有被占用的frame_id, replacer 中记录的是 page_id, pages中存储的是每一个page的具体数据&meta data
 }
 
@@ -36,7 +36,6 @@ BufferPoolManager::~BufferPoolManager() {
   delete replacer_;
 }
 
-// todo: fetch之后是不需要pin and unpin
 Page *BufferPoolManager::FetchPageImpl(page_id_t page_id) {
   std::lock_guard<std::mutex> lock(latch_);
 
@@ -46,13 +45,13 @@ Page *BufferPoolManager::FetchPageImpl(page_id_t page_id) {
   it = page_table_.find(page_id);
   if (it != page_table_.end()) {
     replacer_->Pin(page_id);
-    pages_[it -> second].pin_count_++;
+    pages_[it->second].pin_count_++;
     return pages_ + it->second;
   }
 
   // 1.2    If P does not exist, find a replacement page (R) from either the free list or the replacer.
   //        Note that pages are always found from the free list first.
-  if (free_list_.empty() && replacer_ -> Size() == 0) {
+  if (free_list_.empty() && replacer_->Size() == 0) {
     return nullptr;
   }
   frame_id_t replace_frame;
@@ -60,7 +59,7 @@ Page *BufferPoolManager::FetchPageImpl(page_id_t page_id) {
     replace_frame = free_list_.front();
     free_list_.pop_front();
   } else {
-     replacer_ -> Victim(&replace_frame);
+    replacer_->Victim(&replace_frame);
   }
 
   // 2.     If R is dirty, write it back to the disk.
@@ -76,8 +75,6 @@ Page *BufferPoolManager::FetchPageImpl(page_id_t page_id) {
   pages_[replace_frame].page_id_ = page_id;
   pages_[replace_frame].is_dirty_ = false;
   pages_[replace_frame].pin_count_ = 1;
-  replacer_->Unpin(page_id);
-  replacer_->Pin(page_id);
   disk_manager_->ReadPage(page_id, pages_[replace_frame].data_);
   return pages_ + replace_frame;
 }
@@ -88,12 +85,13 @@ bool BufferPoolManager::UnpinPageImpl(page_id_t page_id, bool is_dirty) {
   if (it == page_table_.end()) {
     return false;
   }
-  frame_id_t frame_id = it -> second;
+  frame_id_t frame_id = it->second;
   bool flag;
   flag = pages_[frame_id].GetPinCount() > 0;
   pages_[frame_id].is_dirty_ = is_dirty;
-  pages_[frame_id].pin_count_ = 0;  // should est to 0, not --
+  pages_[frame_id].pin_count_--;  // todo: what if the pin count is negative?
   if (pages_[frame_id].pin_count_ <= 0) {
+    pages_[frame_id].pin_count_ = 0;
     replacer_->Unpin(page_id);  // pin_count == 0才从LRU中将其标为unpin
   }
   return flag;
@@ -116,10 +114,7 @@ Page *BufferPoolManager::NewPageImpl(page_id_t *page_id) {
   page_id_t temp_page_id = disk_manager_->AllocatePage();
   frame_id_t frame_id;
   // 1.   If all the pages in the buffer pool are pinned, return nullptr.
-  for (size_t i = 0; i < pool_size_; i++) {
-    if () // todo
-  }
-  if (replacer_->Size() == 0) {
+  if (replacer_->Size() == 0 && free_list_.empty()) {
     return nullptr;
   }
   // 2.   Pick a victim page P from either the free list or the replacer. Always pick from the free list first.
@@ -127,9 +122,14 @@ Page *BufferPoolManager::NewPageImpl(page_id_t *page_id) {
     frame_id = free_list_.front();
     free_list_.pop_front();
   } else {
-    replacer_ -> Victim(&frame_id);
+    replacer_->Victim(&frame_id);
   }
   // 3.   Update P's metadata, zero out memory and add P to the page table.
+  page_table_.emplace(temp_page_id, frame_id);
+  pages_[frame_id].ResetMemory();
+  pages_[frame_id].page_id_ = temp_page_id;
+  pages_[frame_id].pin_count_ = 1;  // todo 1 or 0?
+  pages_[frame_id].is_dirty_ = false;
   // 4.   Set the page ID output parameter. Return a pointer to P.
   *page_id = temp_page_id;
   return pages_ + frame_id;
@@ -137,11 +137,24 @@ Page *BufferPoolManager::NewPageImpl(page_id_t *page_id) {
 
 bool BufferPoolManager::DeletePageImpl(page_id_t page_id) {
   // 0.   Make sure you call DiskManager::DeallocatePage!
+  disk_manager_->DeallocatePage(page_id);
   // 1.   Search the page table for the requested page (P).
   // 1.   If P does not exist, return true.
+  std::unordered_map<page_id_t, frame_id_t>::iterator it;
+  it = page_table_.find(page_id);
+  if (it == page_table_.end()) {
+    return true;
+  }
+  frame_id_t frame_id = it->second;
   // 2.   If P exists, but has a non-zero pin-count, return false. Someone is using the page.
+  if (pages_[frame_id].pin_count_ > 0) {
+    return false;
+  }
   // 3.   Otherwise, P can be deleted. Remove P from the page table, reset its metadata and return it to the free list.
-  return false;
+ page_table_.erase(page_id);
+ pages_[frame_id].page_id_ = INVALID_PAGE_ID;
+ free_list_.emplace_back(static_cast<int>(frame_id));
+ return true;
 }
 
 void BufferPoolManager::FlushAllPagesImpl() {
